209501fbe59579a861bde0b66883ffa4ddaa7a60
Omar <omgebeev@mail.ru>
2023-06-04 18:58:41 +0300

    4 june test

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const App = {
       return {
         placeholderString: 'Введите название',
         title: 'Список',
-        inputValue: ''
+        inputValue: '',
+        notes: ['Заметка 1', 'Заметка 2', 'Заметка 3']
       }
     },
     methods: {
       inputChangeHandler(event) {
         this.inputValue = event.target.value
+     },
+     addNewNote () {
+      this.notes.push(this.inputValue)
+      this.inputValue = ''
+     },
+     removeNote() {
+      this.notes.splice(index, 1)
      }
     }
   }
